a705e8891f8364e8fa18d6724351cc6b259141be
anatolich.work.space@gmail.com <anatolich.work.space@gmail.com>
2021-05-22 03:34:10 +0300

    Hello uses style.css

--- index.php
+++ index.php
@@ -5,11 +5,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link type="text/css" rel="stylesheet" media="all" href="lib/style.css">
     <title>HTML tags</title>
 </head>
 <body>
 
-<iframe src="lib/hello.html" width="200" height="200"></iframe>
+<h1>Hello friend</h1>
 
 </body>
 </html>
